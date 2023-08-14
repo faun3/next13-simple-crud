238a3e122f57265b426044e44a75c0eaa1a1246a
faun3 <antonioculcea@gmail.com>
2023-08-14 16:22:35 +0300

    add a get all topics route

--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -1,10 +1,23 @@
 import { NextResponse } from "next/server";
 import { connect } from "@/utils/db";
-import Topic from "@/models/Topic";
+import Topic, { ITopic } from "@/models/Topic";
+import { HydratedDocument } from "mongoose";
 
+// make a new topic
 export async function POST(req: Request) {
   const { title, desc } = await req.json();
   await connect();
   await Topic.create({ title, desc });
   return NextResponse.json({ message: "topic created" }, { status: 201 });
+}
+
+// get all topic
+export async function GET(req: Request) {
+  try {
+    await connect();
+    const foundDocuments: HydratedDocument<ITopic>[] = await Topic.find();
+    return NextResponse.json(foundDocuments, { status: 200 });
+  } catch (err) {
+    return new NextResponse("db error", { status: 500 });
+  }
 }

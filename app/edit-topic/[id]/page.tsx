cd6549b817788c8cfd1c0039eb86cb432630472d
faun3 <antonioculcea@gmail.com>
2023-08-14 14:23:14 +0300

    edit page complete

---
+++ app/edit-topic/[id]/page.tsx
@@ -0,0 +1,11 @@
+import EditTopicForm from "@/components/EditTopicForm";
+
+const EditTopic = () => {
+  return (
+    <div>
+      stuff below
+      <EditTopicForm />
+    </div>
+  );
+};
+export default EditTopic;

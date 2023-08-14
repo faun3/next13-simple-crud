dc8ef27585c9ab377eee1e9a34175b174fa3b342
faun3 <antonioculcea@gmail.com>
2023-08-14 16:17:22 +0300

    add working patch route

--- app/api/topics/[id]/route.ts
+++ app/api/topics/[id]/route.ts
@@ -1,6 +1,7 @@
 import { connect } from "@/utils/db";
 import Topic, { ITopic } from "@/models/Topic";
 import { NextResponse } from "next/server";
+import { HydratedDocument } from "mongoose";
 
 export async function GET(
   req: Request,
@@ -13,6 +14,33 @@ export async function GET(
       return NextResponse.json({ message: "topic not found" }, { status: 500 });
     }
     return NextResponse.json(foundTopic, { status: 200 });
+  } catch (err) {
+    return new NextResponse("db error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connect();
+    const { title, desc } = await req.json();
+
+    const foundTopic: HydratedDocument<ITopic> | null = await Topic.findById(
+      params.id
+    );
+
+    if (!foundTopic) {
+      return NextResponse.json({ message: "topic not found" }, { status: 500 });
+    }
+
+    foundTopic.desc = desc;
+    foundTopic.title = title;
+
+    await foundTopic.save();
+
+    return NextResponse.json({ message: { title, desc } }, { status: 200 });
   } catch (err) {
     return new NextResponse("db error", { status: 500 });
   }

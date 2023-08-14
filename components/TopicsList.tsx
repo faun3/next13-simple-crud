3d4225da8cc04ff2f01530605a64bb4e371be51a
faun3 <antonioculcea@gmail.com>
2023-08-14 13:44:57 +0300

    style todo

--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -4,12 +4,12 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const TopicsList = () => {
   return (
-    <div>
+    <div className="flex flex-row justify-between border-2 rounded-lg my-8 border-black px-4 py-2">
       <div>
         <p>Topic Title</p>
         <div>Topic Description</div>
       </div>
-      <div>
+      <div className="flex flex-row">
         <RemoveBtn />
         <Link href={"/editTopic/123"}>
           <HiPencilAlt size={24} />

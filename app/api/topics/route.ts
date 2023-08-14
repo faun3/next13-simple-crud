1d58cb6129adaecfbe0c682d5413d40078200504
faun3 <antonioculcea@gmail.com>
2023-08-14 15:19:53 +0300

    write POST route to add new document

--- app/api/topics/route.ts
+++ app/api/topics/route.ts
@@ -1,11 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
-import { ITopic } from "@/models/Topic";
+import { NextResponse } from "next/server";
 import { connect } from "@/utils/db";
 import Topic from "@/models/Topic";
 
 export async function POST(req: Request) {
   const { title, desc } = await req.json();
   await connect();
-  Topic.create({ title, desc });
+  await Topic.create({ title, desc });
   return NextResponse.json({ message: "topic created" }, { status: 201 });
 }

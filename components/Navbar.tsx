8d2328221d8528a997f71988285dfe0134eec6d9
faun3 <antonioculcea@gmail.com>
2023-08-14 13:34:17 +0300

    add tailwind classes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/add-topic"}>Add topic</Link>
+    <nav className="flex flex-row justify-between bg-slate-800 text-slate-200 px-4 py-4 rounded-xl items-center">
+      <Link href={"/"} className="cursor-pointer">
+        Home
+      </Link>
+      <Link
+        href={"/add-topic"}
+        className="bg-emerald-600 rounded-lg px-4 py-2 text-slate-200 font-bold text-md">
+        Add topic
+      </Link>
     </nav>
   );
 };

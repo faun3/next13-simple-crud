5d275d5ad5fc679c954b35b8710f714a59d8182c
faun3 <antonioculcea@gmail.com>
2023-08-14 14:23:34 +0300

    create new edit form component

---
+++ components/EditTopicForm.tsx
@@ -0,0 +1,36 @@
+const EditTopicForm = () => {
+  return (
+    <form
+      action=""
+      className="w-full md:w-[60%] mx-auto my-8 rounded-lg border-2 border-slate-900 py-2 px-4 bg-slate-200">
+      <div>
+        <label htmlFor="title" className="block">
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Make food"
+          className="rounded-md px-4 py-1 w-full"
+        />
+      </div>
+      <div>
+        <label htmlFor="desc" className="block">
+          Description
+        </label>
+        <input
+          type="text"
+          name="desc"
+          id="desc"
+          placeholder="Cook some delicious tacos"
+          className="rounded-md px-4 py-1 w-full"
+        />
+      </div>
+      <button className="block mx-auto my-4 bg-emerald-600 text-slate-100 font-bold text-lg px-4 py-2 rounded-lg">
+        Edit
+      </button>
+    </form>
+  );
+};
+export default EditTopicForm;

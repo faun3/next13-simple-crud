632e04a05cc495319fddf881797336b4b5c37b48
faun3 <antonioculcea@gmail.com>
2023-08-14 13:47:14 +0300

    style delete button

--- components/RemoveBtn.tsx
+++ components/RemoveBtn.tsx
@@ -1,4 +1,10 @@
+import { HiOutlineTrash } from "react-icons/hi";
+
 const RemoveBtn = () => {
-  return <div>RemoveBtn</div>;
+  return (
+    <button className="text-red-500">
+      <HiOutlineTrash size={24} />
+    </button>
+  );
 };
 export default RemoveBtn;

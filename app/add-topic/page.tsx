1ef7eb227f3c3a322fc87c1526872f6171ea6ea7
faun3 <antonioculcea@gmail.com>
2023-08-15 20:14:15 +0300

    write new topic logic

--- app/add-topic/page.tsx
+++ app/add-topic/page.tsx
@@ -1,8 +1,38 @@
+"use client";
+
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
 const page = () => {
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const title = formData.get("title") as string;
+    const desc = formData.get("desc") as string;
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, desc }),
+      });
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("failed to create a topic");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <form
       action=""
-      className="w-full md:w-[60%] mx-auto my-8 rounded-lg border-2 border-slate-900 py-2 px-4 bg-slate-200">
+      className="w-full md:w-[60%] mx-auto my-8 rounded-lg border-2 border-slate-900 py-2 px-4 bg-slate-200"
+      onSubmit={handleSubmit}
+    >
       <div>
         <label htmlFor="title" className="block">
           Title

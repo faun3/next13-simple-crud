63c944717fa49ab7bcd94d5ecbb46120d71a1e5a
faun3 <antonioculcea@gmail.com>
2023-08-15 22:07:23 +0300

    write deletion logic

--- components/RemoveBtn.tsx
+++ components/RemoveBtn.tsx
@@ -1,8 +1,36 @@
-import { HiOutlineTrash } from "react-icons/hi";
+"use client";
 
-const RemoveBtn = () => {
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+
+const RemoveBtn = ({ id }: { id: string }) => {
+  const router = useRouter();
+
+  const handleDelete = async (id: string) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "DELETE",
+      });
+      console.log(res);
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("the response for deletion was not ok");
+      }
+    } catch (err) {
+      console.error(err);
+      throw new Error("delete operation failed");
+    }
+  };
+
   return (
-    <button className="text-red-500">
+    <button
+      className="text-red-500"
+      onClick={() => {
+        handleDelete(id);
+      }}
+    >
       <HiOutlineTrash size={24} />
     </button>
   );
